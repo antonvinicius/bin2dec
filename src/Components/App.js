cb0d511263b37af43bfb9685587a0f4d1c715eb0
Vinicius <antoniovrdacosta@gmail.com>
2020-09-28 10:52:44 -0400

    Creating components

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Header from "./Header"
+import BinaryInput from "./BinaryInput"
+import DecimalInput from "./DecimalInput"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <BinaryInput />
+      <DecimalInput />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/DecimalInput.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function DecimalInput() {
+    return (
+        <h1>DecimalInput</h1>
+    )
+}
+
+export default DecimalInput
